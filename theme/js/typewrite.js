c34d3776f605ada38d072171428289010c4e3097
SKM MBP7 <developer@saungkyo.com>
2018-06-30 13:15:02 +0630

    accepted

--- theme/js/typewrite.js
+++ theme/js/typewrite.js
@@ -1,5 +1,5 @@
 // set up text to print, each item in array is new line
-var aText = new Array("ခ်စ္ရပါေသာ...", " ပူရွိန္း ထံသုိ႔");
+var aText = new Array("ခ်စ္ရပါေသာ...", " (- အမည္ထည့္ပါ -) ထံသုိ႔");
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
