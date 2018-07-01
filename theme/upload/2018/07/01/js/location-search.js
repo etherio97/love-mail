4b7e9fb64d235491e33280ab4c3ec19387fb3923
SKM MBP7 <developer@saungkyo.com>
2018-07-01 18:32:02 +0630

    location search fixed... js

--- theme/upload/2018/07/01/js/location-search.js
+++ theme/upload/2018/07/01/js/location-search.js
@@ -1,16 +1,13 @@
-var ls = function () {
+var method = lsOutput(0), recipient = lsOutput(1);
+
+function lsOutput(x){
+    var result = search(x);
+    return result[1];
+}
+
+function search(x) {
     var a = location.search;
     var b = a.slice(1);
-
-    return b.split("&");
+        a = b.split("&");
+    return a[x].split("=");
 }
-
-function lsMethod(num) {
-    //* String Method on Location Search 
-    var method = {0:"decode", 1:"encode"};
-    
-    return method[num];
-}
-
-var method = lsMethod();
-var param = ls();
