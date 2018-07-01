2371d8d718bdb8d720e0ee7ab75515e1a19b9e63
SKM MBP7 <developer@saungkyo.com>
2018-07-01 16:58:03 +0630

    string location search

--- theme/upload/2018/07/01/js/location-search.js
+++ theme/upload/2018/07/01/js/location-search.js
@@ -1,2 +1,11 @@
-var ls = location.search;
+var recipient = function () {
+    ls = location.search;
     ls = ls.slice(1);
+    ls = ls.split("&");
+    return ls;
+}
+
+function lsMethod() {
+    //* String Method on Location Search 
+
+}
