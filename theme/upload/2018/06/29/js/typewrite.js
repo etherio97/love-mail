130e72c70473f66bbd3666e08a257bdb89380516
SKM MBP7 <developer@saungkyo.com>
2018-07-01 18:40:15 +0630

    a

--- theme/upload/2018/06/29/js/typewrite.js
+++ theme/upload/2018/06/29/js/typewrite.js
@@ -1,17 +1,18 @@
 var audio = document.querySelector("#background")
 
 // set up text to print, each item in array is new line
-var aText = new Array("ခ်စ္ရပါေသာ...", recipient + " ထံသုိ႔");
-var iSpeed = 100; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
-
-var iTextPos = 0; // initialise text position
-var sContents = ""; // initialise contents variable
-var iRow; // initialise current row
-
 function typewriter() {
+  var recipt = decodeURI.decodeURIComponent(recipient);
+  var aText = new Array("ခ်စ္ရပါေသာ...", recipt + " ထံသုိ႔");
+  var iSpeed = 100; // time delay of print out
+  var iIndex = 0; // start printing array at this posision
+  var iArrLength = aText[0].length; // the length of the text array
+  var iScrollAt = 20; // start scrolling up at this many lines
+
+  var iTextPos = 0; // initialise text position
+  var sContents = ""; // initialise contents variable
+  var iRow; // initialise current row
+
   sContents = " ";
   iRow = Math.max(0, iIndex - iScrollAt);
   var destination = document.getElementById("typedtext");
